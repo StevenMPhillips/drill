aaf0451b0bd45e32dcdb8069e701f227cfb7cc7c
Steven Phillips <smp@apache.org>
2015-04-20 15:26:43 -0700

    add assertions to streaming agg

--- exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/aggregate/StreamingAggTemplate.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/aggregate/StreamingAggTemplate.java
@@ -44,6 +44,7 @@ public abstract class StreamingAggTemplate implements StreamingAggregator {
   private StreamingAggBatch outgoing;
   private FragmentContext context;
   private boolean done = false;
+  private long totalAdded = 0;
 
 
   @Override
@@ -75,6 +76,8 @@ public abstract class StreamingAggTemplate implements StreamingAggregator {
   public AggOutcome doWork() {
     if (done) {
       outcome = IterOutcome.NONE;
+      long inputCount = outgoing.getTotalIncoming();
+      assert totalAdded == inputCount : String.format("Number of added records does not match incoming records: incoming: %d added: %d", inputCount, totalAdded);
       return AggOutcome.CLEANUP_AND_RETURN;
     }
     try { // outside loop to ensure that first is set to false after the first run.
@@ -330,6 +333,7 @@ public abstract class StreamingAggTemplate implements StreamingAggregator {
   private void addRecordInc(int index) {
     addRecord(index);
     this.addedRecordCount++;
+    assert ++totalAdded <= outgoing.getTotalIncoming() : String.format("Number of added records greater than incoming records: incoming: %d added: %d", outgoing.getTotalIncoming(), totalAdded);
   }
 
   @Override
