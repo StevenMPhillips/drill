3a164691a19a1bc02038beba4e642a8a3eae6643
Steven Phillips <steven@dremio.com>
2016-02-19 14:01:22 -0800

    more wip

---
+++ exec/java-exec/src/main/java/org/apache/drill/exec/vector/complex/RepeatedListVectorHelper.java
@@ -0,0 +1,56 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.drill.exec.vector.complex;
+
+
+import io.netty.buffer.DrillBuf;
+import org.apache.drill.exec.expr.BasicTypeHelper;
+import org.apache.drill.exec.expr.TypeHelper;
+import org.apache.drill.exec.proto.UserBitShared.SerializedField;
+import org.apache.drill.exec.record.MaterializedField;
+import org.apache.drill.exec.record.SerializedFieldHelper;
+import org.apache.drill.exec.vector.BaseRepeatedValueVectorHelper;
+import org.apache.drill.exec.vector.ValueVector;
+
+import java.util.List;
+
+public class RepeatedListVectorHelper {
+  private RepeatedListVector repeatedListVector;
+
+  public RepeatedListVectorHelper(RepeatedListVector vector) {
+    this.repeatedListVector = vector;
+  }
+
+  public void load(SerializedField metadata, DrillBuf buf) {
+    new DelegateRepeatedVectorHelper(repeatedListVector.delegate).load(metadata, buf);
+  }
+
+  public SerializedField getMetadata() {
+    return new DelegateRepeatedVectorHelper(repeatedListVector.delegate).getMetadata();
+  }
+
+  private static class DelegateRepeatedVectorHelper extends BaseRepeatedValueVectorHelper {
+
+    private BaseRepeatedValueVector baseRepeatedValueVector;
+
+    public DelegateRepeatedVectorHelper(BaseRepeatedValueVector vector) {
+      super(vector);
+      this.baseRepeatedValueVector = vector;
+    }
+  }
+}
